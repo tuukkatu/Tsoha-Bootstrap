d8feaf46389164c4d2e0cd87234757cdd25bcfb0
Tuukka Turunen <tuukkatu@dhcp-asv-113.helsinki.fi>
2017-03-30 18:24:10 +0300

    readme linkit paivitetty

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Yleisiä linkkejä:
 
 * [Tietokantasovellus](http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/)
 * [Dokumentaatio](https://github.com/tuukkatu/Tsoha-Bootstrap/blob/master/doc/dokumentaatio.pdf)
-* [Listaussivu](http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/beer_list)
-* [Esittelysivu](http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/show)
-* [Muokkaussivu](http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/edit)
+* [Listaussivu](http://http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/suunnitelmat/frontpage)
+* [Esittelysivu](http://http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/suunnitelmat/1)
+* [Muokkaussivu](http://http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/suunnitelmat/1/edit)
 * [Loginsivu](http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/login)
-* [Etusivu](http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/frontpage)
+* [Etusivu](http://http://tuukkatu.users.cs.helsinki.fi/tietokantasovellus/suunnitelmat/frontpage)
 
 ## Työn aihe
 
--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,41 +1,40 @@
 <?php
 
-/*
+
   class HelloWorldController extends BaseController{
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tämä on etusivu!';
-    }
-
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      //View::make('helloworld.html');
-      $Koff = Olut::find(1);
-      $olut = Olut::all();
-      
-      Kint::dump($olut);
-      Kint::dump($Koff);
-    }
-    
-    public static function esittely(){
-      View::make('suunnitelmat/esittely.html');  
-    }
-    
-    public static function listaussivu(){
-      View::make('suunnitelmat/listaussivu.html');  
-    }
-    
+//    public static function index(){
+//      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+//   	  echo 'Tämä on etusivu!';
+//    }
+//
+//    public static function sandbox(){
+//      // Testaa koodiasi täällä
+//      //View::make('helloworld.html');
+//      $Koff = Olut::find(1);
+//      $olut = Olut::all();
+//      
+//      Kint::dump($olut);
+//      Kint::dump($Koff);
+//    }
+//    
+//    public static function esittely(){
+//      View::make('suunnitelmat/esittely.html');  
+//    }
+//    
+//    public static function listaussivu(){
+//      View::make('suunnitelmat/listaussivu.html');  
+//    }
+//    
     public static function login(){
       View::make('suunnitelmat/login.html');  
     }
     
-    public static function edit(){
-      View::make('suunnitelmat/edit.html');  
-    }
-    
-    public static function frontpage(){
-      View::make('suunnitelmat/frontpage.html');  
-    }
+//    public static function edit(){
+//      View::make('suunnitelmat/edit.html');  
+//    }
+//    
+//    public static function frontpage(){
+//      View::make('suunnitelmat/frontpage.html');  
+//    }
   }
-*/
